9c8f5c4bc8649b35b839c6d8b920dcdb18f032ac
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:31:19 +0500

    Add hollywood-website\src\components\Footer.js

---
+++ hollywood-website\src\components\Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-200 py-4">
+      <p className="container mx-auto p-4 text-center">
+        &copy; 2023 Hollywood Website
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
